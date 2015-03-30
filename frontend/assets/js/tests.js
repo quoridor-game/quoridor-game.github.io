fabf5ffe901f1ce76280c8323efd7c08e1b00ebe
rozaev <rozaev.viktor@ya.ru>
2015-03-30 20:41:06 +0300

    minor

--- frontend/assets/js/tests.js
+++ frontend/assets/js/tests.js
@@ -44,3 +44,13 @@ QUnit.test("click test", function (assert) {
     assert.ok(start.top.getNeighbor(start).domElement.hasClass("-first-player"), "Passed!");
 
 });
+
+QUnit.test("isPathExist test", function (assert) {
+    firstPlayer.position.left.domElement.find("div:first-child").trigger("click");
+    firstPlayer.position.right.domElement.find("div:first-child").trigger("click");
+    var a = firstPlayer.position.top.getNeighbor(firstPlayer.position);
+    a.left.secondCrossover.domElement.trigger("click");
+
+    assert.ok(!firstPlayer.position.top.domElement.hasClass("wall-fill"), "Passed!");
+
+});
